018379832cc023b17b46f155b203fe45d71b3c33
Daniel Freedman <dfreedm@google.com>
2013-06-11 16:54:02 -0700

    all async

--- tasks/audit.js
+++ tasks/audit.js
@@ -9,17 +9,71 @@
 'use strict';
 
 module.exports = function(grunt) {
+  var crypto = require('crypto');
+  var path = require('path');
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
-
-  grunt.registerMultiTask('audit', 'Your task description goes here.', function() {
+  grunt.registerMultiTask('audit', 'Generate audit trail.', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      punctuation: '.',
-      separator: ', '
+      separator: grunt.util.linefeed,
+      repos: []
     });
 
+    function fileHash(filepath) {
+      var blob = grunt.file.read(filepath);
+      var sha1sum = crypto.createHash('sha1');
+      sha1sum.update(blob);
+      var hex = sha1sum.digest('hex');
+      return filepath + ' ' + hex;
+    }
+
+    function findRev(repoPath, callback) {
+      grunt.util.spawn({
+        cmd: 'git',
+        args: ['--git-dir', path.resolve(repoPath, '.git'), 'rev-parse', 'HEAD']
+      }, function(error, result, code) {
+        if (error) {
+          callback(error);
+        } else {
+          callback(null, path.basename(path.resolve(repoPath)) + ' ' + result);
+        }
+      });
+    }
+
+    function repoRevs(repos, callback) {
+      grunt.util.async.map(repos, findRev, function(err, results) {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, results);
+        }
+      });
+    }
+
+    function out(revs, hashes, dest) {
+      // build audit log
+      var out = [
+        'AUDIT LOG ' + grunt.template.today('isoDateTime'),
+        '---------',
+        '',
+        'REPO REVISIONS',
+        '==============',
+        revs.join(options.separator),
+        '',
+        'BUILD HASHES',
+        '============',
+        hashes
+      ].join(options.separator);
+
+      if (dest) {
+        grunt.file.write(dest, out);
+        grunt.log.writeln('Wrote audit log:', dest, 'successfully');
+      } else {
+        grunt.log.writeln(out);
+      }
+    }
+
+    var done = this.async();
     // Iterate over all specified file groups.
     this.files.forEach(function(f) {
       // Concat specified files.
@@ -31,20 +85,21 @@ module.exports = function(grunt) {
         } else {
           return true;
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+      }).map(fileHash).join(options.separator);
+      grunt.util.async.waterfall([
+        function(callback) {
+          var fn = function(err, result) {
+            callback(err, result);
+          }
+          repoRevs(f.repos, fn);
+        },
+        function(repos, callback) {
+          out(repos, src, f.dest);
+          callback();
+        }
+      ], function(err, result) {
+        done(err);
+      });
     });
   });
-
 };
